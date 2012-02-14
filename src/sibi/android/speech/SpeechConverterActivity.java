caaa98795668f998c1f4ee80504a1c4d0a683e11
Sibi <psibi2000@gmail.com>
2012-02-14 20:01:17 +0530

    Complete languages present in the Android SDK supported

--- src/sibi/android/speech/SpeechConverterActivity.java
+++ src/sibi/android/speech/SpeechConverterActivity.java
@@ -1,22 +1,31 @@
 package sibi.android.speech;
 
+import java.util.Arrays;
 import java.util.Locale;
 
 import android.app.Activity;
+import android.app.LocalActivityManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 import android.util.Log;
 
-public class SpeechConverterActivity extends Activity implements TextToSpeech.OnInitListener {
+public class SpeechConverterActivity extends Activity implements TextToSpeech.OnInitListener, OnItemSelectedListener {
     private static final int SPEECH_API_CHECK = 0;
     private TextToSpeech mTts;
     private Button speak;
     private EditText text;
+    private Spinner lang;
+    long position = 0;
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,21 @@ public class SpeechConverterActivity extends Activity implements TextToSpeech.On
         
         speak = (Button)findViewById(R.id.button1);
         text = (EditText) findViewById(R.id.editText1);
+        lang = (Spinner) findViewById(R.id.spinner1);
+        
+        Locale[] locale = Locale.getAvailableLocales();
+        String langs[] = new String[locale.length];
+        for (int i=0;i<locale.length;i++)
+        {
+        	langs[i]=locale[i].getDisplayLanguage() + " " + locale[i].getDisplayName();
+        }
+       
+        
+        ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, langs);
+        lang.setAdapter(spinnerArrayAdapter);
+    
+        lang.setOnItemSelectedListener(this);
+        
         speak.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -39,6 +63,8 @@ public class SpeechConverterActivity extends Activity implements TextToSpeech.On
 
 			private void speak() {
 				// TODO Auto-generated method stub
+				Locale[] locale = Locale.getAvailableLocales();
+				mTts.setLanguage(locale[(int) position]);		
 				String data=text.getText().toString();
 				mTts.speak(data, TextToSpeech.QUEUE_FLUSH, null);
 			}
@@ -88,5 +114,24 @@ public class SpeechConverterActivity extends Activity implements TextToSpeech.On
             // Initialization failed.
             Log.e("app", "Could not initialize TextToSpeech.");
         }
+	}
+
+
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+		// TODO Auto-generated method stub
+		long selected_position = lang.getSelectedItemId();
+		position = (int) selected_position;
+		
+		//mTts.setLanguage(locale	);
+		
+	}
+
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
